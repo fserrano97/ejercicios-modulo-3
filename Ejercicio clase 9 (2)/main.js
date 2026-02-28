e5c203fb766bc3fccc146a1d0ccf8bd46b7d2f79
fserrano97-pro <florenciaayelenserrano@gmail.com>
2026-02-28 16:51:25 -0300

    Actidad 2 clase 9

---
+++ Ejercicio clase 9 (2)/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".navbar-burger").addEventListener("click", function () {
+    document.querySelector(".navbar-burger").classList.toggle("is-active");
+    document.querySelector(".navbar-menu").classList.toggle("is-active");}); 
